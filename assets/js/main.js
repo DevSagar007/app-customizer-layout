cd0cdb3cdbbc49f6670769ea4e6ea804d71f1a32
DevSagar007 <sagorkhan9294@gmail.com>
2025-08-07 01:13:29 +0600

    update sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 // ==================================================
-// * Project Name   :
-
+// * Project Name   : App Customizer layout
 // * File           :  JS Base
 // * Version        :  1.0
 // * Last change    :  24 june 2025, tuesday
-// * Author         :  
+// * Author         :
 // ==================================================
 
 (function ($) {
   ("use strict");
 
+  // Initialize notice show toggle
   $(".notice-show-toggle").click(function () {
     let noticeText = $(".notice-text");
 
@@ -25,20 +25,20 @@
     }
   });
 
-  // Wait for the DOM to be fully loaded
+  // Initialize app layout js
   document.addEventListener("DOMContentLoaded", function () {
-    const menuItems = document.querySelectorAll(".customize-menu-item");
+    const menuItems = document.querySelectorAll(".app-sidebar-menu-item");
     const optionsPanel = document.querySelector(".app-customize-area");
     const backButton = document.querySelector(".sidebar-customize-back");
 
-    // Add 'active' class when any menu item is clicked
+    // Add 'active' class
     menuItems.forEach((item) => {
       item.addEventListener("click", function () {
         optionsPanel.classList.add("active");
       });
     });
 
-    // Remove 'active' class when back button is clicked
+    // Remove 'active' class
     backButton.addEventListener("click", function () {
       optionsPanel.classList.remove("active");
     });
